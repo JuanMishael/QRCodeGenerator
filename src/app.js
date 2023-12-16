2efbc57d7375aab9877fb4431bb22f04b243e3bb
Ju4nMishael <johnmishaelparcal@gmail.com>
2023-12-17 00:00:56 +0800

    Completed Project
    Added Output QR Code SVG

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import qr from 'qr-image';
 const app = express();
 const PORT = 3300;
 
@@ -8,15 +9,32 @@ app.use(bodyParser.urlencoded({extended:true}))
 // parse application/json
 app.use(bodyParser.json())
 
+const noOutput = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="1" height="1">
+        <rect width="1%" height="1%" fill="transparent"/>
+        <text x="50%" y="50%" font-size="20" fill="black" text-anchor="middle" alignment-baseline="middle"></text>
+    </svg>
+`;
+
 
 app.get('/',(req,res)=>{
-    res.render('../view/index.ejs')
+    res.render('../view/index.ejs',{
+        output: noOutput
+    })
 })
 
 app.get('/submit-form', (req,res) =>{
-    const submittedText = req.query.textToQR;
-    console.log('Submitted Text:', submittedText);
-    res.send('Form submitted successfully!');
+    try {
+        const submittedText = req.query.textToQR;
+        const svg_string = qr.imageSync(submittedText, { type: 'svg' });
+        res.render('../view/index.ejs',{
+            output: encodeURIComponent(svg_string)
+        });
+    } catch (error) {
+        res.render('../view/index.ejs',{
+            output: noOutput
+        })
+    } 
 })
 
 app.listen( PORT, () => {
